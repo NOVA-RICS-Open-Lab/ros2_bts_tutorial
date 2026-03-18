fbe82c6ca84f3a9767f2bee6b5f5ba5e997734bd
altsmpegado <altsmpegado@gmail.com>
2026-03-18 19:13:55 +0100

    Update bt_tutorial_05.hpp

--- include/bt_tutorial_05.hpp
+++ include/bt_tutorial_05.hpp
@@ -29,4 +29,34 @@ private:
   int _pick_attempts = 0;
 };
 
+// Help for Exercise 5
+class DecodeDoorCode : public BT::SyncActionNode
+{
+public:
+  DecodeDoorCode(const std::string& name, const BT::NodeConfiguration& config);
+  static BT::PortsList providedPorts();
+  BT::NodeStatus tick() override;
+};
+
+class UseDoorCode : public BT::SyncActionNode
+{
+public:
+  UseDoorCode(const std::string& name, const BT::NodeConfiguration& config, CrossDoor& cross_door);
+  static BT::PortsList providedPorts();
+  BT::NodeStatus tick() override;
+
+private:
+  CrossDoor& cross_door_;
+};
+
+class LogResult : public BT::SyncActionNode
+{
+public:
+  LogResult(const std::string& name, const BT::NodeConfiguration& config);
+  static BT::PortsList providedPorts();
+  BT::NodeStatus tick() override;
+};
+
+void registerPasswordNodes(BT::BehaviorTreeFactory& factory, CrossDoor& cross_door);
+
 #endif // ROS2_BT_TUT__BT_TUT_05_HPP_
